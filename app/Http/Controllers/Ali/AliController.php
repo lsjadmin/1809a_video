3680274ac1397696640524322281a78279af2d64
lsjadmin <3023668879@qq.com>
2019-07-16 16:12:43 +0800

    deug

--- app/Http/Controllers/Ali/AliController.php
+++ app/Http/Controllers/Ali/AliController.php
@@ -12,7 +12,6 @@ class AliController extends Controller
         $json=file_get_contents("php://input");
         $log_str=date("Y-m-d H:i:s").'>>>>'.$json."\n";
         file_put_contents("logs/oss.log",$log_str,FILE_APPEND);
-
     }
 
 
